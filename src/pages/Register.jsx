fc1076434c8f22f1824622c5306db7fb5605b4c2
Waris Punnahitanond <waris46842@hotmail.com>
2023-03-31 01:32:42 +0700

    add pages: Home, Login, Register, Header

---
+++ src/pages/Register.jsx
@@ -0,0 +1,60 @@
+import {useState} from 'react';
+import {toast} from 'react-toastify';
+import { FaUser } from 'react-icons/fa';
+
+function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const {name,email,password,password2} = formData;
+
+    const onChange = (e) => {
+        setFormData((prevState)=>({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if(password!==password2) {
+            toast.error('Passwords do not match')
+        }
+    }
+
+    return (
+        <>
+            <section className='heading'>
+                <h1>
+                    <FaUser/>Register
+                </h1>
+                <p>Please create an account</p>
+            </section>
+            <section className='form'>
+                <form onSubmit={onSubmit}>
+                    <div className='form-group'>
+                        <input type='text' className='form-groupinput' id='name' name='name' value={name} onChange={onChange} placeholder='Enter Your name' required/>
+                    </div>
+                    <div className='form-group'>
+                        <input type="email" className='form-groupinput' id='email' name='email' value={email} onChange={onChange} placeholder='Enter Your email' required/>
+                    </div>
+                    <div className='form-group'>
+                        <input type="password" className='form-groupinput' id='password' name='password' value={password} onChange={onChange} placeholder='Enter Your password' required/>
+                    </div>
+                    <div className='form-group'>
+                        <input type="password" className='form-groupinput' id='password2' name='password2' value={password2} onChange={onChange} placeholder='Confirm Your password' required/>
+                    </div>
+                    <div className='form-group'>
+                        <button className='btn btn-block'>Submit</button>
+                    </div>
+                </form>
+            </section>
+        </>
+    )
+}
+
+export default Register;
